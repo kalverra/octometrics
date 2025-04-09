bec4e8133af293a7da01ff6e4daa49a7a02b9f15
Adam Hamrick <adam.hamrick@smartcontract.com>
2025-04-09 16:54:21 -0400

    Fixes test

--- monitor/analyze_test.go
+++ monitor/analyze_test.go
@@ -13,7 +13,18 @@ func TestAnalyze(t *testing.T) {
 
 	log, _ := testhelpers.Setup(t)
 	require.FileExists(t, "testdata/monitor.log.json", "monitor log file does not exist")
-	_, err := Analyze(log, "testdata/monitor.log.json")
+	analysis, err := Analyze(log, "testdata/monitor.log.json")
 	require.NoError(t, err, "error analyzing monitor log")
-	t.Fatal("not implemented fully")
+	require.NotNil(t, analysis, "analysis should not be nil")
+
+	cpuMeasurements := analysis.CPUMeasurements
+	require.NotNil(t, cpuMeasurements, "cpu measurements should not be nil")
+	require.NotEmpty(t, cpuMeasurements, "cpu measurements should not be empty")
+
+	for _, measurements := range cpuMeasurements {
+		require.NotNil(t, measurements, "measurements should not be nil")
+		require.NotEmpty(t, measurements, "measurements should not be empty")
+	}
+
+	// TODO: Add more tests and better assertions on exact data expected
 }
